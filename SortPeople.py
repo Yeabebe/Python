8b926cbd101cbe3cd144e11a4417e0e19959860b
Yeabseera Abebe <yeabebe41@gmail.com>
2025-04-09 11:07:50 +0300

    List to list

--- SortPeople.py
+++ SortPeople.py
@@ -1,4 +1,4 @@
 class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+    def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
         sorted_people = sorted(zip(heights, names), reverse=True)
         return [name for _, name in sorted_people]
