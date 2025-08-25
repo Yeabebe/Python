cab8dbb7d551b90bea3e2c2a49b373fdbad785ce
Yeabseera Abebe <yeabebe41@gmail.com>
2025-08-25 08:13:14 +0300

    Combination Sum

---
+++ combinationSum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path, total):
+            # Base case: if total == target, we found a valid combination
+            if total == target:
+                res.append(path[:])
+                return
+            # If total exceeds, stop exploring
+            if total > target:
+                return
+
+            # Explore candidates starting from 'start' 
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])
+                # Backtrack 
+                path.pop()
+
+        backtrack(0, [], 0)
+        return res
