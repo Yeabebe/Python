79924cde97839de124840a3dc9f44b50ec8936f8
Yeabseera Abebe <yeabebe41@gmail.com>
2025-08-29 07:19:38 +0300

    Permutations

---
+++ permutations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        res = []
+        for i in range(len(nums)):
+            n = nums[i]
+            remaining = nums[:i] + nums[i+1:]
+            for perm in self.permute(remaining):
+                res.append([n] + perm)
+        return res
