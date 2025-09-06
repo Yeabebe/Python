3f8599a82388119212d956f7dc9c61e0ac6ce30e
Yeabseera Abebe <yeabebe41@gmail.com>
2025-09-06 08:09:46 +0300

    Jump game

---
+++ jumpGame2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i, jump in enumerate(nums):
+            if i > farthest:  # we are stuck, can't reach this position
+                return False
+            farthest = max(farthest, i + jump)
+        return True
