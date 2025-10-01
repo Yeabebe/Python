7088097d53aeec0ffb26d57e851bafe132c23653
Yeabseera Abebe <yeabebe41@gmail.com>
2025-10-01 20:43:39 +0300

    Finding square root

---
+++ squarroot.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        guess = x
+        while guess * guess > x:
+            guess = (guess + x // guess) // 2
+        return guess
