1bd7483e5f968c94d63e6e6cf56ae0240fb405c2
Yeabseera Abebe <yeabebe41@gmail.com>
2025-10-15 08:00:40 +0300

    Partition list

---
+++ partitionList.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        # Dummy nodes for two partitions
+        before_head = ListNode(0)
+        after_head = ListNode(0)
+        
+        before = before_head
+        after = after_head
+        current = head
+        
+        # Traverse and divide nodes
+        while current:
+            if current.val < x:
+                before.next = current
+                before = before.next
+            else:
+                after.next = current
+                after = after.next
+            current = current.next
+        
+        # End the after list and connect two parts
+        after.next = None
+        before.next = after_head.next
+        
+        return before_head.next
