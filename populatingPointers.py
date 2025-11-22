6913d608c0710481270b462fac7cc2a24268bca3
Yeabseera Abebe <yeabebe41@gmail.com>
2025-11-22 12:14:52 +0300

    Populating next right pointers in each node

---
+++ populatingPointers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        if root.left:
+            root.left.next = root.right
+
+            if root.next:
+                root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+
+        return root
+        
