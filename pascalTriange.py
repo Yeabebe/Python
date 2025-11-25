26c1563a784531ac56cb0dad4276a40f183bfdc0
Yeabseera Abebe <yeabebe41@gmail.com>
2025-11-25 10:00:18 +0300

    Pascal triangle

---
+++ pascalTriange.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+
+            if i > 0:
+                for j in range(1, i):
+                    row[j] = res[i-1][j-1] + res[i-1][j]
+            res.append(row)
+        return res
+        
+
+        
