13a60998ad8ec1ce9c8f15907627311b3421676e
Yeabseera Abebe <yeabebe41@gmail.com>
2025-12-17 06:53:09 +0300

    Sum root to leaf numbers

---
+++ sumRoot.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, current):
+            if not node:
+                return 0
+            
+            # build the number
+            current = current * 10 + node.val
+            
+            # if leaf, return the number formed
+            if not node.left and not node.right:
+                return current
+            
+            # sum from left and right subtrees
+            return dfs(node.left, current) + dfs(node.right, current)
+        
+        return dfs(root, 0)
