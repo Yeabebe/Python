04d0b3a25cf03fd61a2f72fde0805863e16e2882
Yeabseera Abebe <yeabebe41@gmail.com>
2025-12-23 08:24:50 +0300

    Single number II

---
+++ singleNumberII.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        
+        for i in range(32):
+            bit_sum = 0
+            for num in nums:
+                bit_sum += (num >> i) & 1
+            
+            if bit_sum % 3:
+                result |= (1 << i)
+        
+        # handle negative numbers (32-bit signed integer)
+        if result >= 2**31:
+            result -= 2**32
+        
+        return result
