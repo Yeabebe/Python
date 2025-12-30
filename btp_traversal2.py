fd0a41c3ae97c8c0f1046e69a9978bb275363a77
Yeabseera Abebe <yeabebe41@gmail.com>
2025-12-30 07:47:57 +0300

    Binary tree postorder traversal

---
+++ btp_traversal2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)     # Left
+            dfs(node.right)    # Right
+            result.append(node.val)  # Root
+        
+        dfs(root)
+        return result
