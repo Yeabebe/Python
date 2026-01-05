a8e78da6be0c4acb987b52f8c9d78febccbd84ac
Yeabseera Abebe <yeabebe41@gmail.com>
2026-01-05 14:27:24 +0300

    Maximum product subarray

---
+++ maximum_product.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_prod = min_prod = result = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+
+            # If num is negative, swap max and min
+            if num < 0:
+                max_prod, min_prod = min_prod, max_prod
+
+            max_prod = max(num, max_prod * num)
+            min_prod = min(num, min_prod * num)
+
+            result = max(result, max_prod)
+
+        return result
