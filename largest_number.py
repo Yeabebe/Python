c775545c0a73ad4d9e3a78e2da0a77055ad0a863
Yeabseera Abebe <yeabebe41@gmail.com>
2026-01-30 07:26:57 +0300

    Largest number

---
+++ largest_number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        strs = list(map(str, nums))
+
+        def compare(a, b):
+            if a + b > b + a:
+                return -1
+            elif a + b < b + a:
+                return 1
+            else: 
+                return 0
+
+        strs.sort(key = cmp_to_key(compare))
+        
+        result = ''.join(strs)
+
+        return "0" if result[0] == "0" else result
