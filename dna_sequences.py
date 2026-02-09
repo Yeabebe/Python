3372044c41d6b5ee3ecc61ed19fffe1adbfeb7f1
Yeabseera Abebe <yeabebe41@gmail.com>
2026-02-09 11:27:23 +0300

    Repeated DNA sequences

---
+++ dna_sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen = defaultdict(int)
+        res = []
+
+        for i in range(len(s) - 9):
+            sub = s[i:i+10]
+            seen[sub] += 1
+            if seen[sub] == 2:   # add only once
+                res.append(sub)
+
+        return res
