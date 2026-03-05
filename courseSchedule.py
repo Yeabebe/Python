6eb10243895c43ae7571e641f9f164bbde50e0b3
Yeabseera Abebe <yeabebe41@gmail.com>
2026-03-05 10:53:09 +0300

    Course schedule II

---
+++ courseSchedule.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)  
+            indegree[a] += 1
+        queue = deque()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        order = []
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        if len(order) == numCourses:
+            return order
+
+        return []
