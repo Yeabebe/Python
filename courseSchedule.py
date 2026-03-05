6c996624912eebd24659088cb58c69daf48f21db
Yeabseera Abebe <yeabebe41@gmail.com>
2026-03-05 10:54:49 +0300

    Import libraries

--- courseSchedule.py
+++ courseSchedule.py
@@ -1,3 +1,6 @@
+from collections import defaultdict, deque
+from typing import List
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
